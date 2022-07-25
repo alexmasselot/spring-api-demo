ff58230ce0747706c3c53890b0e20e85f1a0e488
Alexandre Masselot <alexandre.masselot@gmail.com>
2022-07-25 12:20:41 +0200

    hello says paf le chien

---
+++ src/main/kotlin/com/loccitane/api/demo/controllers/HelloController.kt
@@ -0,0 +1,17 @@
+package com.loccitane.api.demo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+@author Alexandre Masselot
+@Copyright L'Occitane 2022
+ */
+@RestController
+class HelloController {
+    @GetMapping("/greeting")
+    fun home(): String {
+        return "paf le chien"
+    }
+}
